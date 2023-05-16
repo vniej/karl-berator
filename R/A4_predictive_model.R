5d6fb701bf9f41813c1c633e6e5b21e05bde3e65
vniej <vivienne.niejalke@student.adelaide.edu.au>
2023-05-16 14:57:17 +0930

    I can make an IMRaD report

--- R/A4_predictive_model.R
+++ R/A4_predictive_model.R
@@ -1,9 +1,27 @@
-#make a predictive model for Karl and the IMRaD report
+#make a predictive model for the IMRaD report
 
-#we are going to use the lme4 and lmerTest package
-pacman::p_load(tidyverse,ggplot,lmerTest,lme4)
+#we are going to use the lme4 package
+pacman::p_load(ggplot,lmerTest,lme4)
 
 #read in data
-gene<-read.csv("C:/Users/AHS Student/Desktop/KarlBerator/data/2023_03_21-gene_expression.csv")
+gene<-read.csv(here::here("karl-berator/data/2023_03_21-gene_expression.csv"))
 
 #fit model with lmer
+model_lme4<-lmer(growth_factor~(concentration|gene_expression)+ concentration+
+                   treatment,
+                 data=gene)
+
+#summary statistics and assumption checking
+summary(model_lme4)
+plot(model_lme4)
+
+#plot predictive model
+ggplot(gene,aes(x=concentration,y=growth_factor,color=gene_expression))+
+  geom_point()+
+  geom_line(aes(y=predict(model_lme4)))+
+  labs(
+    x="\u00b5g/ml",
+    y="Growth Factor (units)"
+  ) + ggtitle("Predictive Model for Gene Expression")
+
+
